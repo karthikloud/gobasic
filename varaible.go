6e37168942d35be0489cc4ec4c4bff4ce4907364
karthikloud <127289548+karthikloud@users.noreply.github.com>
2023-03-16 13:04:59 +0530

    Add files via upload

---
+++ varaible.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var age int = 22
+	var name string = "ariv"
+	var isMarried bool = false
+	var weight float64 = 55.5
+
+	// Print the values
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Married:", isMarried)
+	fmt.Println("Weight:", weight)
+
+	// Declare variables
+	var height int
+	var occupation string
+
+	// Assign values
+	height = 6
+	occupation = "Software Developer"
+
+	// Print the values
+	fmt.Println("Height:", height)
+	fmt.Println("Occupation:", occupation)
+
+}
