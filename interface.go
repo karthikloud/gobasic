716cafefe124ee29af2c6fcadc71f34e7d49bfc4
karthikloud <127289548+karthikloud@users.noreply.github.com>
2023-03-20 09:24:30 +0530

    Add files via upload

---
+++ interface.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Shape interface {
+	area() float64
+}
+
+type Square struct {
+	side float64
+}
+
+func (s Square) area() float64 {
+	return s.side * s.side
+}
+
+func printArea(s Shape) {
+	fmt.Println("Area:", s.area())
+}
+
+func main() {
+	square := Square{side: 5}
+	printArea(square)
+}
